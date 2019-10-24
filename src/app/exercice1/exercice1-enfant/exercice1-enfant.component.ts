a5fd3124065096081cd3f39fd62c2644ca786ba1
prak <prak.david@gmail.com>
2019-10-25 00:29:07 +0200

    exercice 1

--- src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
+++ src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-exercice1-enfant',
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exercice1-enfant.component.css']
 })
 export class Exercice1EnfantComponent implements OnInit {
+  public result : number = 0;
+  @Output() update = new EventEmitter();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  incrementer() {
+    this.update.emit(++this.result);
+  }
+
+  decrementer() {
+    this.update.emit(--this.result);
+  }
 }
